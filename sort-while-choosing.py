7478373a14ec003d69bb1c2159015310bd561885
bhuvannarula <55291162+bhuvannarula@users.noreply.github.com>
2019-09-14 00:42:02 +0530

    sort-while-choosing

--- sort-while-choosing.py
+++ sort-while-choosing.py
@@ -2,16 +2,15 @@
 This code sorts the data while choosing (main aim).
 '''
 
-x=eval("["+input("Enter with comma: ")+"]")         #to accept more than 2 digit no.
-y=eval("["+input("Enter with comma: ")+"]")
+x=eval("["+input("Enter with comma: ")+"]")                     #to accept more than 2 digit no.
+y=eval("["+input("Enter with comma: ")+"]") + [float('inf')]    #inf is used only to compare
 x.sort()
 y.sort()
-y.append(float('inf'))
 for i in x:
     s=0
     for j in y:
         if j>=i:
-            y.insert(s,i)                           #editing the original list
+            y.insert(s,i)                                       #editing the original list
             break
         s+=1
 print(y[0:(len(y)-1)])
