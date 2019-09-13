8baa98985ad3dc9f652c3e9f3524132e016fa892
bhuvannarula <55291162+bhuvannarula@users.noreply.github.com>
2019-09-13 22:03:30 +0530

    Create sort-while-choosing.py

---
+++ sort-while-choosing.py
@@ -0,0 +1,12 @@
+x=eval("["+input("Enter with comma: ")+"]")         #to accept more than 2 digit no.
+y=eval("["+input("Enter with comma: ")+"]")
+x.sort()
+y.sort()
+for i in x:
+    s=0
+    for j in y:
+        if j>=i:
+            y.insert(s,i)                           #editing the original list
+            break
+        s+=1
+print(y)
